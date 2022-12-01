28ed560b766893ca40f5c6c016598d78048d9d96
Sara Park <sspark106@gmail.com>
2022-11-30 19:47:16 -0700

    Add code to book

--- src/book.js
+++ src/book.js
@@ -1,12 +1,48 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  var character = {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  }
+  return character;
+};
+
+function saveReview(review, array) {
+  var reviews = array;
+  if (reviews.includes(review) === false) {
+    reviews.push(review);
+  }
+};
+
+function calculatePageCount(bookTitle) {
+  var letterCount = bookTitle.length;
+  return letterCount * 20;
+}
+
+function writeBook(bookTitle, bookCharacter, bookGenre) {
+  var book = {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    pageCount: calculatePageCount(bookTitle),
+    genre: bookGenre
+  }
+  return book;
+};
+
+function editBook(book) {
+  book.pageCount = book.pageCount * 0.75
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
+
