33b42ab8e8749d69d9a1a69d0320943189ba7616
Sara Park <sspark106@gmail.com>
2022-12-01 17:45:39 -0700

    Add code for shelfBook and unshelfBook

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,33 @@
+function shelfBook(book, shelf) {
+  if (shelf.length < 3) {
+    if (book.genre === "sciFi") {
+      shelf.unshift(book);
+  } }
+};
+
+function unshelfBook(book, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === book) {
+      var indexOfBook = shelf.indexOf(shelf[i]);
+      return shelf.splice(indexOfBook, 1); 
+    }
+  }
+};
+
+function listTitles(array) {
+  for (var i = 0; i < array.length; i++) {
+  }
+};
+
+
+
+
+
+
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
+  shelfBook,
+  unshelfBook,
+  listTitles,
   // searchShelf
 };
