4b5156fcb6a9d5fe23314408929e7a404a7a4a41
Sara Park <sspark106@gmail.com>
2022-12-02 18:26:57 -0700

    Add code to library

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,30 @@
+function createLibrary(name) {
+  return {
+    name: name,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: []
+    }
+  };
+};
+
+function addBook(object1, object2) {
+  object1.shelves[object2.genre].push(object2);
+};
+
+function checkoutBook(name, title, genre) {
+  if (name.shelves[genre].length > 0) {
+    name.shelves[genre].pop();
+    return `You have now checked out ${title} from the ${name.name}`
+  } else {
+    return `Sorry, there are currently no copies of ${title} available at the ${name.name}`
+  }
+};
 
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook
 };
