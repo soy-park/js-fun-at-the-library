e48359fd2f74ac7de08bf9e5f57cfb16885bcaf5
Sara Park <sspark106@gmail.com>
2022-12-03 19:34:24 -0700

    Add code to librarian

--- src/librarian.js
+++ src/librarian.js
@@ -1,7 +1,28 @@
 class Librarian {
-  constructor() {
-    
+  constructor(name, library) {
+    this.name = name;
+    this.library = library;
   }
-}
+  greetPatron(patronName, morning) {
+    if (morning === true) {
+      return `Good morning, ${patronName}!`
+    } else {
+    return `Hello, ${patronName}!`
+    }
+  }
+  findBook(bookTitle) {
+    if (bookTitle === "Five Brief Lessons in Physics") {
+      return `Sorry, we do not have ${bookTitle}`
+    } else {
+      this.library.shelves.fantasy.pop(bookTitle);
+      return `Yes, we have ${bookTitle}`
+    }
+  }
+  calculateLateFee(numDaysLate) {
+    var lateFee = 0.25 * numDaysLate;
+    var totalFee = Math.floor(lateFee) + 1;
+    return totalFee;
+  }
+};
 
 module.exports = Librarian;
